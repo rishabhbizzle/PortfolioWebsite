f892f6bd5454068c5db8d0137060bc77b7d12bc8
rishabhbizzle <rishabhbizzle@gmail.com>
2024-01-26 01:18:21 +0530

    feat: new project added

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -5,6 +5,7 @@ import SpotApp from "../assets/spot2.png";
 import Pos from "../assets/pos.png";
 import DataProject from "../assets/dataproject.png";
 import BBcharts from "../assets/bbcharts.png";
+import ResumeLint from "../assets/resumeLint.png";
 
 const Projects = () => {
   return (
@@ -91,7 +92,29 @@ const Projects = () => {
               </div>
             </div>
           </div>
-
+          <div
+            style={{ backgroundImage: `url(${ResumeLint})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+          >
+            {/* Hover Effects */}
+            <div className="opacity-0 group-hover:opacity-100">
+              <span className="text-2xl font-bold text-white tracking-wider">
+                ResumeLint
+              </span>
+              <div className="pt-8 text-center">
+                <a href="https://resumelint.netlify.app/">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Demo
+                  </button>
+                </a>
+                <a href="https://github.com/rishabhbizzle/resumelint">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
           <div
             style={{ backgroundImage: `url(${BBcharts})` }}
             className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
@@ -139,29 +162,6 @@ const Projects = () => {
             </div>
           </div>
           {/* Grid Item */}
-          <div
-            style={{ backgroundImage: `url(${QuoteApp})` }}
-            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
-          >
-            {/* Hover Effects */}
-            <div className="opacity-0 group-hover:opacity-100">
-              <span className="text-2xl font-bold text-white tracking-wider">
-                Quotes Generator
-              </span>
-              <div className="pt-8 text-center">
-                <a href="https://anime-insta-captions.netlify.app/">
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Demo
-                  </button>
-                </a>
-                <a href="https://github.com/rishabhbizzle/anime-quote-generator">
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Code
-                  </button>
-                </a>
-              </div>
-            </div>
-          </div>
         </div>
       </div>
     </div>
