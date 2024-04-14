85a52e29daff11525d30b19e8dce6b1a3d33f42a
rishabhbizzle <rishabhbizzle@gmail.com>
2024-04-14 23:51:47 +0530

    [update] statscrave added to project list

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -30,15 +30,15 @@ const Projects = () => {
             {/* Hover Effects */}
             <div className="opacity-0 group-hover:opacity-100">
               <span className="text-2xl font-bold text-white tracking-wider">
-                SpotAnalytics (SFF)
+                StatsCrave
               </span>
               <div className="pt-8 text-center">
-                <a href="https://statsforfans.azurewebsites.net/">
+                <a href="https://statscrave.com/">
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                     Link
                   </button>
                 </a>
-                <a href="https://github.com/rishabhbizzle/spotAnalytics">
+                <a href="https://github.com/rishabhbizzle/statscrave">
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                     Code
                   </button>
