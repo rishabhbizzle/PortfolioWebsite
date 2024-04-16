0c74bd7b6c95d3f09f15c791cbbc9dfc3c202b76
rishabhbizzle <rishabhbizzle@gmail.com>
2024-04-16 23:58:37 +0530

    [fix] name of project

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -146,7 +146,7 @@ const Projects = () => {
             {/* Hover Effects */}
             <div className="opacity-0 group-hover:opacity-100">
               <span className="text-2xl font-bold text-white tracking-wider">
-                Data Analysis Project
+                Anime Data Analysis Project
               </span>
               <div className="pt-8 text-center">
                 <a href="https://github.com/rishabhbizzle/anime-data-analysis">
